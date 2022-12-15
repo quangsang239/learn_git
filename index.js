43d0565d5f6c9198ca4b3a940c1f6bf89aa099a2
quangsang239 <quangsang239@gmail.com>
2022-12-15 08:24:39 +0700

    handle 2 function in branch master

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function handleConflict(a, b) {
-  return a + b;
+  let conflict = (b - a) * 20;
+  return conflict;
 }
 function writingText(text) {
-  console.log(text);
+  let info = text + " Sinh ngày: 26/08/2001";
+  console.log(info);
 }
 console.log(handleConflict(1, 2));
 writingText("Quang Sang 239");
